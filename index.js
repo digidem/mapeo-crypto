11295922187bbc1643a51c76dec653906e9c73b8
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-09-06 09:53:21 +0100

    chore!: remove ProjectInvites
    Fixes #9. ProjectInvites are not used in any current code.
    We are now inviting over RPC, not QR codes or URLs. We can add something
    back later if needed.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const { sign, verifySignature, projectKeyToPublicId } = require('./utils.js')
 
 exports.KeyManager = require('./key-manager')
-exports.invites = require('./project-invites')
 exports.sign = sign
 exports.verifySignature = verifySignature
 exports.projectKeyToPublicId = projectKeyToPublicId
